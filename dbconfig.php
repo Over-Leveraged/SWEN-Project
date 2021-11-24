abf415bc193429751858369f282884c8c10d7430
Tahjay Thompson <76020197+TahjayThompson@users.noreply.github.com>
2021-11-24 14:08:46 -0500

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'records';
+    $username = 'admin';
+    $password = 'password123';
+
+// used to create rights to database
+/*GRANT ALL PRIVILEGES ON records.* TO 'admin'@'localhost' IDENTIFIED BY 'password123';*/

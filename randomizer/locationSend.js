288b07c45192254631ceef162fa5af1090c00e55
Jelani Smith <rathalosrocks@gmail.com>
2021-11-26 18:26:55 -0500

    Initial randomizer

---
+++ randomizer/locationSend.js
@@ -0,0 +1,37 @@
+window.onload=function(){
+    var search = document.getElementById("officerRandomizer");
+    search.addEventListener('click',handleClick);
+
+}
+
+var httpRequest = new XMLHttpRequest();
+var url = "randomizer.php";
+
+function handleClick(e){
+
+    e.preventDefault();
+    
+
+    var url = "randomizer.php";
+    //var location= document.getElementById("fillerForLocationName").value;
+   // location=location.trim();
+    
+    //var sendToPhp=url+"?query="+location;
+
+    httpRequest.onreadystatechange = getList();
+
+    httpRequest.open('GET',url)
+    httpRequest.send();
+}
+
+function getList(){
+    var resField=document.getElementById('result');
+    if (httpRequest.readyState == XMLHttpRequest.DONE){
+        if(httpRequest.status==200){
+            var resp= httpRequest.responseText;
+            resField.innerHTML=resp;
+        }else{
+            alert("No officers found.");
+        }
+    }
+}

69545c1288d0f3d9b57d3d9ba0c69c11cf1f6d93
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-27 03:42:14 -0500

    completed exporter

--- database/records.sql
+++ database/records.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 24, 2021 at 05:31 AM
+-- Generation Time: Nov 24, 2021 at 08:03 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -16,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/* GRANT ALL PRIVILEGES ON records.* TO 'admin'@'project.com' IDENTIFIED BY 'password123';*/
+
 --
 -- Database: `records`
 --
@@ -29,20 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `addRec` (
   `id` int(11) NOT NULL,
-  `fname` varchar(250) NOT NULL,
-  `lname` varchar(250) NOT NULL,
-  `company` varchar(250) NOT NULL,
-  `position` varchar(250) NOT NULL,
-  `service_lenght` varchar(250) NOT NULL,
-  `avsec_dates` date NOT NULL,
-  `avsec_grades` varchar(250) NOT NULL,
-  `medical_doc` blob NOT NULL,
-  `medical_doc_expire` date NOT NULL,
-  `psra_doc` blob NOT NULL,
-  `psra_doc_expire` date NOT NULL,
-  `police_rec_doc` blob NOT NULL,
-  `police_rec_doc_expire` date NOT NULL,
-  `recommenedBy` varchar(250) NOT NULL
+  `fname` varchar(250) DEFAULT NULL,
+  `lname` varchar(250) DEFAULT NULL,
+  `company` varchar(250) DEFAULT NULL,
+  `position` varchar(250) DEFAULT NULL,
+  `service_lenght` varchar(250) DEFAULT NULL,
+  `avsec_dates` date DEFAULT NULL,
+  `avsec_grades` varchar(250) DEFAULT NULL,
+  `medical_doc` varchar(255) DEFAULT NULL,
+  `medical_doc_path` varchar(255) DEFAULT NULL,
+  `medical_doc_expire` date DEFAULT NULL,
+  `psra_doc` blob DEFAULT NULL,
+  `psra_doc_path` varchar(255) DEFAULT NULL,
+  `psra_doc_expire` date DEFAULT NULL,
+  `police_rec_doc` blob DEFAULT NULL,
+  `police_rec_doc_path` varchar(255) DEFAULT NULL,
+  `police_rec_doc_expire` date DEFAULT NULL,
+  `recommenedBy` varchar(250) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,6 +77,16 @@ ALTER TABLE `addRec`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addRec`
+--
+ALTER TABLE `addRec`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

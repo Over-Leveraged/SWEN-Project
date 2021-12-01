b0c0cae82f9c1f950b94d0fb6351d9bef7a38ccc
tahjaythompsonm <thompsontahjay34@gmail.com>
2021-12-01 16:05:20 -0500

    added export,upload,delete and create php <JS needed for delete and and saving records>

---
+++ export.php
@@ -0,0 +1,42 @@
+<?php 
+ 
+// Load the database configuration file 
+include_once('model/dbconfig.php'); 
+
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // echo "Connected to $dbname at $host successfully.";    
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+
+
+?>
+
+            
+<form method='post' action='download.php'>
+<button type="submit" id="export_csv_data" name='export_csv_data' value="Export to CSV" >Export to CSV</button>
+        
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>fname</th>
+         
+        </tr>
+<?php 
+$result = $conn->query("SELECT * FROM addRec ORDER BY id ASC"); 
+$eachh= $result->fetchAll(PDO::FETCH_ASSOC);
+$records[] =$eachh;
+?>
+
+
+<?php foreach ($eachh as $place): ?>
+    <tr>
+        <td> <?= $place['id']; ?></td>  
+        <td> <?= $place['fname']; ?></td>  
+    </tr>
+<?php endforeach; ?>
+</table>
+</form>
+
